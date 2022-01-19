66ac115fbf7f38e4663abeb0a9f7721eb3fb93ea
dekeey <dekeeytp@gmail.com>
2022-01-19 14:15:34 +0300

    Ex2: Aggregate Staking Rewards

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,6 +1,26 @@
 import { SubstrateEvent } from '@subql/types'
-import { StakingReward } from '../types'
+import { StakingReward, SumReward } from '../types'
 import { Balance } from '@polkadot/types/interfaces'
+
+function createSumReward(accountId: string): SumReward {
+    const entity = new SumReward(accountId)
+    entity.totalReward = BigInt(0)
+    return entity
+}
+export async function handleSumRewarded({
+    block,
+    event: {
+        data: [account, newReward],
+    },
+}: SubstrateEvent): Promise<void> {
+    let entity = await SumReward.get(account.toString())
+    if (entity === undefined) {
+        entity = createSumReward(account.toString())
+    }
+    entity.totalReward = entity.totalReward + (newReward as Balance).toBigInt()
+    entity.blockheight = block.block.header.number.toNumber()
+    await entity.save()
+}
 
 export async function handleStakingRewarded({
     idx,
